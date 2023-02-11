381d93a04799898d0b950dbb4d2899d2b84d86f3
francolivelli <franco.livelli@gmail.com>
2023-02-10 22:45:32 -0300

    global styles

---
+++ client/app/components/TopBanner.jsx
@@ -0,0 +1,7 @@
+import styles from "../../styles/components/TopBanner.module.scss";
+
+const TopBanner = () => {
+  return <div className={styles.banner} ><p>Mensaje promocional</p></div>;
+};
+
+export default TopBanner;
--- client/app/layout.js
+++ client/app/layout.js
@@ -1,9 +1,11 @@
-import "../styles/globals.css"
+import "../styles/globals.scss"
+import TopBanner from "./components/TopBanner"
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
+      <TopBanner/>
       <body>{children}</body>
     </html>
   )
