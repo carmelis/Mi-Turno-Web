1c876c75f66841674b2c4ea2b60f469c0bd16fa6
francolivelli <franco.livelli@gmail.com>
2023-03-01 20:01:00 -0300

    fix

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,15 +1,9 @@
-
 import LoginForm from "./components/LoginForm";
 
 function Home() {
   return (
-<<<<<<< HEAD
     <section className="section-a">
       <LoginForm/>
-=======
-    <section className="section">
-      <LoginForm />
->>>>>>> e4a3553e2228afae97a381c489dd63868d99a03e
     </section>
   );
 }
